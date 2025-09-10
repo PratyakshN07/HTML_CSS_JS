b0e58ccc95fdfa0be76f7b3d828e3d1fd35f419e
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-10 21:43:33 +0530

    JavaScript

---
+++ JavaScript/j.js
@@ -0,0 +1,9 @@
+{
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "[c]": {
+    "editor.defaultFormatter": "ms-vscode.cpptools"
+  },
+  "[css]": {
+    "editor.defaultFormatter": "vscode.css-language-features"
+  }
+}
