a312b37765d65c96c5441771766963f48a14cec4
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-28 12:28:13 +0530

    JavaScript

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -7,3 +7,15 @@
 console.log("Hello World", 4 + 6, "Another Log");
 console.warn("This is warning");
 console.error("This is error");
+
+//3. JavaScript Variables
+//What are Variables? - Containers to store datavales
+var number1 = 34;
+var number2 = 26;
+console.log(number1 + number2);
+
+/*
+Multi 
+Line 
+Comment
+*/
