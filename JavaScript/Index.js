b316504b660f531b6c8d053168e57e0722431250
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-28 13:50:14 +0530

    JavaScript

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -4,15 +4,15 @@
 //document.write("This is a sample")
 
 //2. JavaScript console API
-console.log("Hello World", 4 + 6, "Another Log");
-console.warn("This is warning");
-console.error("This is error");
+// console.log("Hello World", 4 + 6, "Another Log");
+// console.warn("This is warning");
+// console.error("This is error");
 
 //3. JavaScript Variables
 //What are Variables? - Containers to store datavales
 var number1 = 34;
 var number2 = 26;
-console.log(number1 + number2);
+// console.log(number1 + number2);
 
 /*
 Multi 
@@ -36,19 +36,19 @@ var marks = {
   piyush: 76,
   om: 99.8,
 };
-console.log(marks);
+// console.log(marks);
 
 //5. Booleans
 var a = true;
 var b = false;
-console.log(a, b);
+// console.log(a, b);
 
 //6. Undefined
 var und;
-console.log(und);
+// console.log(und);
 
 var n = null;
-console.log(n);
+// console.log(n);
 
 /*
 At a very high level,there are 2 types of datatype in JavaScript
@@ -59,6 +59,34 @@ At a very high level,there are 2 types of datatype in JavaScript
 
 //6. Arrays
 var arr = [1, 2, "Three", 4, 5, 6];
-console.log(arr[2]);
-console.log(arr[0]);
-console.log(arr);
+// console.log(arr[2]);
+// console.log(arr[0]);
+// console.log(arr);
+
+//#7. Operators In JavaScript
+//Arithematic Operators
+var a = 50;
+var b = 25;
+console.log("The value of a+b is", a + b);
+console.log("The value of a-b is", a - b);
+console.log("The value of a*b is", a * b);
+console.log("The value of a/b is", a / b);
+
+//Assignment Operators
+var c = b; //It means C is assigned as variable "b";
+// c += 2;   //c = c + 2;
+// c -= 2;   //c = c - 2;
+// c *= 2;   //c = c * 2;
+// c /= 2;   //c = c / 2;
+// console.log(c);
+
+var x = 12;
+var y = 20;
+// console.log(x != y);
+// console.log(x == y);
+// console.log(x <= y);
+// console.log(x >= y);
+// console.log(x > y);
+// console.log(x < y);
+
+//Logical Operators
